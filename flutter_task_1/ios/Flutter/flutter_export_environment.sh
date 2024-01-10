e2ca8f0886b12896eb93b3d97bc7fb02ef941222
yiheyistm <yiheyisyt23@gmail.com>
2024-01-10 22:02:28 +0300

    Todo List app UI

--- flutter_task_1/ios/Flutter/flutter_export_environment.sh
+++ flutter_task_1/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\Flutter Apps\Todo_List\flutter_task_1"
+export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\Flutter Apps\flutter_task_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- flutter_task_1/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_task_1/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\Flutter Apps\Todo_List\flutter_task_1"
+export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\Flutter Apps\flutter_task_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
